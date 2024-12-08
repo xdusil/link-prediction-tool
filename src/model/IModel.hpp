174aa28c1581af1d5540a8091629e10189953b9e
Jakub Dusil <xdusil@fi.muni.cz>
2024-12-08 13:14:48 +0100

    feat(model): add IModel interface

---
+++ src/model/IModel.hpp
@@ -0,0 +1,54 @@
+#pragma once
+#include <string>
+
+/**
+ * @brief Generic interface for machine learning models.
+
+ * @tparam TInput Type of input data
+ * @tparam TOutput Type of output predictions
+ * @tparam TLoss Type of loss value
+ * @tparam TEmbedding Type of embeddings
+ */
+template <typename TInput, typename TOutput, typename TLoss, typename TEmbedding>
+class IModel {
+public:
+    virtual ~IModel() = default;
+
+    /**
+     * @brief Forward method: Processes input data and produces predictions.
+     *
+     * @param input The input data.
+     * @return The output predictions.
+     */
+    virtual TOutput forward(const TInput &input) = 0;
+
+    /**
+     * @brief Computes the loss for the model.
+     *
+     * @param predictions The model's predictions.
+     * @param input The input data.
+     * @return The loss value.
+     */
+    virtual TLoss loss(const TOutput &predictions, const TInput &input) = 0;
+
+    /**
+     * @brief Returns the model's embeddings.
+     *
+     * @return The model's embeddings.
+     */
+    virtual const TEmbedding &get_embeddings() const = 0;
+
+    /**
+     * @brief Saves the model to a file.
+     *
+     * @param path The file path to save the model.
+     */
+    virtual void save(const std::string &path) const = 0;
+
+    /**
+     * @brief Loads the model from a file.
+     *
+     * @param path The file path to load the model from.
+     */
+    virtual void load(const std::string &path) = 0;
+};
