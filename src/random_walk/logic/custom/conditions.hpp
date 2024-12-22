04cbda7ed3993be978940e72b25946f1d986df3d
Jakub Dusil <xdusil@fi.muni.cz>
2024-12-22 16:56:56 +0100

    refactor(random_walk/logic/custom): update parameter names and use auto

--- src/random_walk/logic/custom/conditions.hpp
+++ src/random_walk/logic/custom/conditions.hpp
@@ -11,66 +11,68 @@ namespace custom_conditions {
 /**
  * @brief Condition 1: expresses the opening of LR dependency
  *
- * @param current The properties of the current edge.
- * @param previous The properties of the previous edge.
+ * @param current_edge The properties of the current edge.
+ * @param previous_edge The properties of the previous edge.
  * @param was_two_steps_ago Whether the target vertex was two steps ago.
  * @return True if the conditions are satisfied, false otherwise.
  */
-inline bool satisfies_LR_opening_conditions(const EdgeProperties &current,
-                                            const EdgeProperties &previous,
+inline bool satisfies_LR_opening_conditions(const auto &current_edge,
+                                            const auto &previous_edge,
                                             bool was_two_steps_ago) {
-    return !was_two_steps_ago && (previous.start_timestamp <= current.start_timestamp &&
-                                  current.start_timestamp <= current.end_timestamp &&
-                                  current.end_timestamp <= previous.end_timestamp);
+    return !was_two_steps_ago &&
+           (previous_edge.start_timestamp <= current_edge.start_timestamp &&
+            current_edge.start_timestamp <= current_edge.end_timestamp &&
+            current_edge.end_timestamp <= previous_edge.end_timestamp);
 }
 
 /**
  * @brief Condition 2: expresses the return from LR dependency
- * 
- * @param current The properties of the current edge.
- * @param previous The properties of the previous edge.
+ *
+ * @param current_edge The properties of the current edge.
+ * @param previous_edge The properties of the previous edge.
  * @param walk_sequence The current walk sequence.
  * @param current_vertex The current vertex.
  * @param was_two_steps_ago Whether the target vertex was two steps ago.
  * @return True if the conditions are satisfied, false otherwise.
  */
-inline bool satisfies_return_from_LR_conditions(const EdgeProperties &current,
-                                                const EdgeProperties &previous,
-                                                const std::vector<Vertex> &walk_sequence,
-                                                const Vertex &current_vertex,
+inline bool satisfies_return_from_LR_conditions(const auto &current_edge,
+                                                const auto &previous_edge,
+                                                const auto &walk_sequence,
+                                                const auto &current_vertex,
                                                 bool was_two_steps_ago) {
     return !was_two_steps_ago &&
            utils::is_vertex_pair_in_sequence_opposite(
                walk_sequence, walk_sequence[walk_sequence.size() - 1], current_vertex) &&
-           (current.start_timestamp <= previous.start_timestamp &&
-            previous.start_timestamp <= previous.end_timestamp &&
-            previous.end_timestamp <= current.end_timestamp);
+           (current_edge.start_timestamp <= previous_edge.start_timestamp &&
+            previous_edge.start_timestamp <= previous_edge.end_timestamp &&
+            previous_edge.end_timestamp <= current_edge.end_timestamp);
 }
 
 /**
  * @brief Condition 3: expresses the opening of RR dependency that should happen
  * within the specified time ε.
  *
- * @param current The properties of the current edge.
- * @param previous The properties of the previous edge.
+ * @param current_edge The properties of the current edge.
+ * @param previous_edge The properties of the previous edge.
  * @param epsilon The time window for the RR dependency.
  * @param was_one_step_ago Whether the target vertex was one step ago.
  * @return True if the conditions are satisfied, false otherwise.
  */
-inline bool satisfies_RR_opening_conditions(const EdgeProperties &current,
-                                            const EdgeProperties &previous, int epsilon,
+inline bool satisfies_RR_opening_conditions(const auto &current_edge,
+                                            const auto &previous_edge, int epsilon,
                                             bool was_one_step_ago) {
-    return previous.end_timestamp <= current.start_timestamp && !was_one_step_ago &&
-           std::chrono::duration_cast<std::chrono::milliseconds>(current.start_timestamp -
-                                                                 previous.end_timestamp)
+    return previous_edge.end_timestamp <= current_edge.start_timestamp &&
+           !was_one_step_ago &&
+           std::chrono::duration_cast<std::chrono::milliseconds>(
+               current_edge.start_timestamp - previous_edge.end_timestamp)
                    .count() <= epsilon;
 }
 
 /**
  * @brief Condition 4: formalizes return over reverse edge representing reverse flow.
  *
- * @param current The properties of the current edge.
- * @param previous The properties of the previous edge.
+ * @param current_edge The properties of the current edge.
+ * @param previous_edge The properties of the previous edge.
  * @param walk_sequence The current walk sequence.
  * @param current_vertex The current vertex.
  * @param was_two_steps_ago Whether the target vertex was two steps ago.
@@ -78,17 +80,16 @@ inline bool satisfies_RR_opening_conditions(const EdgeProperties &current,
  * @return True if the conditions are satisfied, false otherwise.
  */
 inline bool satisfies_return_over_reverse_edge_conditions(
-    const EdgeProperties &current, const EdgeProperties &previous,
-    const std::vector<Vertex> &walk_sequence, const Vertex &current_vertex,
-    bool was_two_steps_ago, int epsilon_rev) {
+    const auto &current_edge, const auto &previous_edge, const auto &walk_sequence,
+    const auto &current_vertex, bool was_two_steps_ago, int epsilon_rev) {
     return was_two_steps_ago &&
            utils::is_vertex_pair_in_sequence_opposite(
                walk_sequence, walk_sequence[walk_sequence.size() - 1], current_vertex) &&
-           (previous.src_port == current.dst_port &&
-            previous.dst_port == current.src_port &&
-            previous.start_timestamp <= current.start_timestamp &&
+           (previous_edge.src_port == current_edge.dst_port &&
+            previous_edge.dst_port == current_edge.src_port &&
+            previous_edge.start_timestamp <= current_edge.start_timestamp &&
             std::abs(std::chrono::duration_cast<std::chrono::milliseconds>(
-                         previous.end_timestamp - current.end_timestamp)
+                         previous_edge.end_timestamp - current_edge.end_timestamp)
                          .count()) <= epsilon_rev);
 }
 
