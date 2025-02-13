88bfe2889a831ad8ae53e661da2743c5f8184ff9
Jakub Dusil <xdusil@fi.muni.cz>
2025-02-13 17:33:08 +0100

    feat(io): FileReader throws an custom exception

--- src/io/FileReader.cpp
+++ src/io/FileReader.cpp
@@ -1,9 +1,10 @@
 #include "FileReader.hpp"
+#include "../exceptions/exceptions.hpp"
 
 // Constructor
 FileReader::FileReader(const std::string& filename)
     : m_file(filename) {
     if (!m_file.is_open()) {
-        throw std::runtime_error("Could not open file: " + filename);
+        throw FileReaderException("Could not open file: " + filename);
     }
 }
