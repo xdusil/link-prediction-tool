8a043b6891970d5d7a2343af9a815d25da7a1b00
Jakub Dusil <xdusil@fi.muni.cz>
2025-12-27 22:48:21 +0100

    refactor(ground_truth): improve pair_hash function with asymmetric hashing

--- src/ground_truth/DependencyAnalyzer.hpp
+++ src/ground_truth/DependencyAnalyzer.hpp
@@ -64,8 +64,11 @@ struct RR3Dependency {
 // Hash function for pairs
 struct pair_hash {
     template <class T1, class T2>
-    std::size_t operator()(const std::pair<T1, T2> &pair) const {
-        return std::hash<T1>()(pair.first) ^ std::hash<T2>()(pair.second);
+    std::size_t operator()(const std::pair<T1, T2> &pair) const noexcept {
+        // Asymmetric hash: (A, B) and (B, A)
+        auto h1 = std::hash<T1>()(pair.first);
+        auto h2 = std::hash<T2>()(pair.second);
+        return h1 ^ (h2 + (h2 << 1)); // h1 XOR (3 * h2)
     }
 };
 
