c8d783195342849ab32643166048a7167b4c9072
Jakub Dusil <xdusil@fi.muni.cz>
2026-05-22 16:53:28 +0200

    feat(app): add scores output path to prediction mode

--- src/main.cpp
+++ src/main.cpp
@@ -26,7 +26,8 @@ int main(int argc, char *argv[]) {
         } else if (args.prediction_mode) {
             app.run_prediction_mode(*args.classifier_path, *args.predictions_output_path,
                                     *args.data_path, args.ground_truth_input_path,
-                                    args.blocked_ips_path, args.internal_ips_path);
+                                    args.blocked_ips_path, args.internal_ips_path,
+                                    args.scores_output_path);
         } else {
             app.run_ground_truth_mode(*args.data_path, *args.ground_truth_output_path,
                                            args.blocked_ips_path);
